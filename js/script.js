0a7c736c511d3fdd12c92a926e78f2dcd6f64dd9
Onamuyiwa <onamuyiwaonabolu>
2026-07-18 23:50:01 +0100

    Add interactive FAQ section with JavaScript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("JavaScript is working!");
+
+const questions = document.querySelectorAll(".faq-question");
+questions.forEach(question => {
+    question.addEventListener("click", () => {
+        const answer = question.nextElementSibling;
+
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+        } else {
+            answer.style.display = "block";
+        }
+    });
+});
